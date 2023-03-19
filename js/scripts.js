ef30e2a0cc3912cb5672f27b7acf1c3d901d76dc
sixstringlove <126977439+sixstringlove@users.noreply.github.com>
2023-03-19 19:08:50 -0400

    added pokemon name, height, type
    first 5 of pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write (favoriteFood);
+let pokemonList = [{name:'Charizard', height:'1.7', type:['Fire, Flying']},
+                    {name:'Ninetails', height:'1.1', type:'Fire'},
+                    {name:'Kingler', height:'1.3', type:'Water'},
+                    {name:'Zapdos', height:'1.6', type: ['Electric, Flying']},
+                    {name:'Aerodactyl', height:'1.8', type: ['Rock, Flying']},
+                ]
+                
+
 
