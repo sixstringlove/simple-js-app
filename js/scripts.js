db521da6f04b7d46fbbe7a8c6e9bd35a28469e28
sixstringlove <126977439+sixstringlove@users.noreply.github.com>
2023-04-13 16:17:43 -0400

    moved function below array
    trying to see if the order matters

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 
-let pokemonRepository = (function() {
-  let repository = [{name:'Charizard', height:'1.7', type:['Fire, Flying']},
+
+let pokemonList = [{name:'Charizard', height:'1.7', type:['Fire, Flying']},
                     {name:'Ninetails', height:'1.1', type:['Fire']},
                     {name:'Kingler', height:'1.3', type:['Water']},
                     {name:'Zapdos', height:'1.6', type: ['Electric, Flying']},
                     {name:'Aerodactyl', height:'1.8', type: ['Rock, Flying']}, 
-  ];
-  let pokemonList = [];
+  ]
+
+  let pokemonRepository = (function() {
+    let pokemonList = [];
 
   function getAll() {
     return pokemonList;
